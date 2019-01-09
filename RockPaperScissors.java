9c82179c8f684584cea4aae6e30b9e1c5dec870f
jisanjung <43708042+jisanjung@users.noreply.github.com>
2019-01-09 01:59:26 -0500

    Updated RockPaperScissors
    added infinite loop and stats to see scoreboard from a .txt file

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,28 +1,47 @@
 import java.util.*;
+import java.io.*;
+import static java.lang.System.*;
+
 public class RockPaperScissors {
 
 	public static void main(String[] args) {
+		int win = 0;
+		int lost = 0;
 		Random r = new Random();
 		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Enter rock, paper, or scissors: ");
-			String userInput = s.nextLine();
-		System.out.println(user(userInput));
-		
-		System.out.println("Computer chooses: ");
-			int cpuInput = r.nextInt(3) + 1;
-		System.out.println(cpu(cpuInput));
-		
-		if ((userInput.equals("rock") && cpuInput == 1) || (userInput.equals("paper") && cpuInput == 2) || (userInput.equals("scissors") && cpuInput == 3)) {
-			System.out.println(" ");
-			System.out.println("TIE");
-		} else if ((userInput.equals("rock") && cpuInput == 2) || (userInput.equals("paper") && cpuInput == 3) || (userInput.equals("scissors") && cpuInput == 1)) {
-			System.out.println(" ");
-			System.out.println("You LOSE!");
-		} else if ((userInput.equals("rock") && cpuInput == 3) || (userInput.equals("paper") && cpuInput == 1) || (userInput.equals("scissors") && cpuInput == 2)) {
-			System.out.println(" ");
-			System.out.println("You WIN!");
-		} 
+		for (int i = 1; i > 0; i++) {
+			System.out.println("Enter rock, paper, scissors, or stats to see scoreboard: ");
+				String userInput = s.nextLine();
+			System.out.println(user(userInput));
+			
+			System.out.println("Computer chooses: ");
+				int cpuInput = r.nextInt(3) + 1;
+			System.out.println(cpu(cpuInput));
+			
+			if ((userInput.equals("rock") && cpuInput == 1) || (userInput.equals("paper") && cpuInput == 2) || (userInput.equals("scissors") && cpuInput == 3)) {
+				System.out.println(" ");
+				System.out.println("TIE");
+			} else if ((userInput.equals("rock") && cpuInput == 2) || (userInput.equals("paper") && cpuInput == 3) || (userInput.equals("scissors") && cpuInput == 1)) {
+				System.out.println(" ");
+				System.out.println("You LOSE!");
+				++lost;
+			} else if ((userInput.equals("rock") && cpuInput == 3) || (userInput.equals("paper") && cpuInput == 1) || (userInput.equals("scissors") && cpuInput == 2)) {
+				System.out.println(" ");
+				System.out.println("You WIN!");
+				++win;
+			} else if (userInput.equals("quit")) {
+				System.out.println("Goodbye");
+				System.exit(0);
+			}
+			fileCreator(win, lost);
+			
+			if (userInput.equals("stats")) {
+				System.out.println("\n" + "****SCOREBOARD****");
+				showStats();
+				System.exit(0);
+			}
+		}
 	}
 	public static String user(String input) {
 		
@@ -43,6 +62,33 @@ public class RockPaperScissors {
 			return "paper";
 		} else {
 			return "scissors";
+		}
+	}
+	public static void fileCreator(int wins, int loss) {
+		try {
+			FileWriter fw = new FileWriter("stats.txt");
+			PrintWriter pw = new PrintWriter(fw);
+			
+			pw.println("Wins: " + wins);
+			pw.println("Losses: " + loss);
+			
+			pw.close();
+		} catch (IOException e) {
+			System.out.println("error");
+		}
+	}
+	public static void showStats() {
+		try {
+			FileReader fr = new FileReader("stats.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			String str;
+			while ((str = br.readLine()) != null) {
+				System.out.println(str + "\n");
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.println("File not found");
 		}
 	}
 }
